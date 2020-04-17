c932dc297891ad8e26e7f3133a190a0cce212ece
dnbaker <dnb@cs.jhu.edu>
2020-04-17 09:38:02 -0400

    Add bias subtraction for PCA. Leverage symmetry in SparseVector code.

--- blaze-rnla/decomp/PCA.h
+++ blaze-rnla/decomp/PCA.h
@@ -11,17 +11,30 @@ namespace blaze {
 
 namespace PCA {
 
+/*
+ * CovarianceAccumulator
+ * This class contains all the methods required for accumulating a covariance calculation
+ * over a stream.
+ * The accumulation_ matrix stores a packed representation of the upper triangle of the matrix
+ * with `dim_ - i` entries each in the ith row, i \in [0, \dots, dim_ - 1],
+ * where we keep the complete sum of all pairwise products of features.
+ *
+ * The means_ uses a running mean calculation for each feature.
+ * Using these, we can calculate the covariance of two features simply via:
+ * C_{ij} = \frac{A_{i} A_{j}}{N - 1} - \mu_i \mu_j
+ */
 template<typename FT, bool TF>
 class CovarianceAccumulator {
     DynamicVector<FT, TF> accumulation_;
     DynamicVector<FT, TF> means_;
     size_t n_added_;
     const size_t dim_;
-    static size_t dim2nelem(size_t n) {
+
+    static constexpr size_t dim2nelem(size_t n) {
         return (n * (n + 1)) / 2;
     }
 public:
-    CovarianceAccumulator(size_t dim): accumulation_(dim2nelem(dim)), means_(dim), n_added_(0), dim_(dim) {}
+    CovarianceAccumulator(size_t dim): accumulation_(dim2nelem(dim), FT(0)), means_(dim), n_added_(0), dim_(dim) {}
     template<typename VT>
     void add(const DenseVector<VT, TF> &v) {
         auto &vr = ~v;
@@ -29,13 +42,13 @@ public:
         if(++n_added_ == 1) { // Use Welford's running sd algorithm for better accuracy
             means_ = vr;
         } else {
-            means_ += (vr - means_) / n_added_;
+            means_ += (vr - means_) * (1. / n_added_);
         }
         size_t subv_length = dim_, offset = 0;
         for(size_t i = 0; i < dim_; ++i) {
-            auto subv = subvector(accumulation_, offset, subv_length);
-            auto vsubv = subvector(vr, i, subv_length);
-            subv += vsubv[0] * vsubv;
+            // In packed representation, [add x_i \cdot {x_i + 1, ..., x_dim}] to covariance matrix accumulation
+            subvector(accumulation_, offset, subv_length) +=
+                vr[i] * subvector(vr, i, subv_length);
             offset += subv_length--;
         }
     }
@@ -46,15 +59,20 @@ public:
         if(++n_added_ == 1) { // Use Welford's running sd algorithm for better accuracy
             means_ = vr;
         } else {
-            means_ += (vr - means_) / n_added_;
+            means_ += (vr - means_) * (1. / n_added_);
         }
-        for(const auto &pair: v) {
-            auto val = pair.value();
-            auto index = pair.index();
-            size_t offset = dim_ * index - ((index - 1) * index);
-            const size_t subv_len = dim_ - index;
-            subvector(accumulation_, offset, subv_len) += subvector(vr, index, subv_len);
-            // Offset into array where the subvector starts
+        // Leverage both sparsity and symmetry
+        for(auto it = vr.begin(), eit = vr.end(); it != eit; ++it) {
+            const auto lhval = it->value();
+            const auto lhind = it->index();
+            size_t lhoffset = dim_ * lhind - ((lhind - 1) * lhind);
+            auto ptr = &accumulation_[dim_ * lhind - ((lhind - 1) * lhind)];
+            auto oit = it;
+            do {
+                const auto rhval = oit->value();
+                const auto rhind = oit->index();
+                ptr[rhind - lhind] += lhval * oit->value();
+            } while(++oit != eit);
         }
     }
     template<typename VT>
@@ -78,10 +96,10 @@ public:
             }
         }
     }
-    DynamicVector<FT, TF> covariance_array() const {
+    DynamicVector<FT, TF> covariance_array(bool subtract_bias=true) const {
         DynamicVector<FT, TF> ret = accumulation_;
         size_t offset = 0, subv_length = dim_;
-        ret *= FT(1. / n_added_);
+        ret *= FT(1. / (n_added_ - subtract_bias));
         for(size_t i = 0; i < dim_; ++i) {
             auto subv = subvector(ret, offset, subv_length);
             subv -= means_[i] * subvector(means_, i, subv_length);
@@ -90,8 +108,8 @@ public:
         return ret;
     }
     template<bool SO=rowMajor>
-    decltype(auto) covariance_matrix() const {
-        auto ca = covariance_array();
+    decltype(auto) covariance_matrix(bool subtract_bias=true) const {
+        auto ca = covariance_array(subtract_bias);
         DynamicMatrix<FT, SO> ret(dim_, dim_);
         size_t offset = 0, subv_length = dim_;
         for(size_t i = 0; i < dim_; ++i) {
@@ -128,6 +146,7 @@ auto pca(const Matrix<MT, SO> &mat, unsigned ncomp=0, bool byrow=true) {
 
 } // PCA
 using PCA::CovarianceAccumulator;
+using PCA::pca;
 
 }
 
